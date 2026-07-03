52b6886f3531a6bb7f56aa55a50477a1d77cd4f3
Erik <133105110+Unsainted38@users.noreply.github.com>
2026-07-03 16:48:31 +0300

    Delete network_transport directory

--- network_transport/abstract_network_transport.cpp
+++
@@ -1,8 +0,0 @@
-#include "abstract_network_transport.h"
-
-AbstractNetworkTransport::AbstractNetworkTransport(QString configPath, QString section, QObject *parent)
-    : QObject{parent},
-      m_configPath(configPath),
-      m_section(section) {
-}
-
